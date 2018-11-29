da0124fa7eead49d232dc7dc6cbf848b4f608bbb
DIVAZAS <44619569+DIVAZAS@users.noreply.github.com>
2018-11-29 03:40:49 -0500

    Verdad o Reto

---
+++ juego/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include "windows.h"
+#include "juego.h"
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+using namespace std;
+using namespace sf;
+int main()
+{
+
+
+
+    //PlaySound(TEXT("musicTF.wav"),NULL,SND_ASYNC);
+    juego *verdadoreto;
+    verdadoreto= new juego(800,600,"Truth or Dare");
+
+
+
+
+
+    return 0;
+}
